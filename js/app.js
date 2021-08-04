cccd08c93a423130a272b37223b177c09ff35f97
bpozniak <benjaminpozniak@gmail.com>
2021-08-03 20:48:03 -0400

    Tamagotchi Progress - Added Image, Added h1/Title, Created buttons, added prompt, and created Tamagotchi class

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("Ben's Tamagotchi")
+
+class Tamagotchi{
+	constructor(name){
+		this.name = name
+		this.hunger = hunger
+		this.sleep = sleep
+		this.bored = bored
+		this.age = age
+
+	}
+}
