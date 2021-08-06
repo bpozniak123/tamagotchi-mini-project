f544e916784d11335031b3d2ee9616ba1e979710
bpozniak <benjaminpozniak@gmail.com>
2021-08-06 14:24:22 -0400

    Progress in formatting attributes, buttons, and image. Created button functions and built game setup

--- js/app.js
+++ js/app.js
@@ -10,108 +10,139 @@ class Tamagotchi{
 	constructor(name){
 		this.name = name
 		this.age = 1
-		this.hunger = 5
-		this.sleep = 3
-		this.boredom = 10
-		this.alive = true
+		this.hunger = 0
+		this.sleep = 0
+		this.boredom = 0
+		this.dies = true
 
 	}
+	updateAttributes(){
+	    let hunger = document.querySelector('#Hunger')
+	    hunger.textContent = `Hunger: ${this.hunger}`
+	    let sleep = document.querySelector('#Sleep')
+	    sleep.textContent = `Sleep: ${this.sleep}`
+	    let boredom = document.querySelector('#Boredom')
+	    boredom.textContent = `Boredom: ${this.boredom}`
+	     let age = document.querySelector('#Age')
+	    age.textContent = `Age: ${this.age}`
+  	};
 	getName(){
 		let displayName = document.querySelector('h2')
 		displayName.textContent = userInput
-	}
+	};
 	getAge(){
 		this.age++
-	}
+	};
 	getHunger(){
 		this.hunger++
-		}
+	};
 	getSleep(){
 		this.sleep++
-		}
+	};
 	getBoredom(){
 		this.boredom++
-	}
+	};
 	getDies(){
-		this.alive = false;
-		clearInterval(begin.timeInt);
-		begin.death();
+		if(this.age===10 || this.hunger===10 || this.sleep===10 || this.boredom===10){
+			this.dies == true
+			alert("GAME OVER!")
+			location.reload()
+		}
+	};
+}
+//-----Setup a new game---------
+class start {
+	startGame(){
+		const start = new Tamagotchi(userInput)
+		let ageVariable = age(start)
+		this.start.getAge()
+		this.start.getHunger()
+		this.start.getSleep()
+		this.start.getBoredom()
+		this.start.getDies()
 	}
-}
 
+//Increase pet's age every X minutes ***COMMIT***
+	// function age(name){
+	// 	return window.setInterval (function(){
+	// 		name.getAge()
+	// 		console.log(name.age)
+	// 	},2000)
+	// },
 
-// Setup a new game
-
-// const begin = {
-// 	timer: 0,
-// 	player: null,
-// 	createName(name) {
-// 		const pizza = new Tamagotchi(name);
-// 		this.player = pizza;
-// 		}
-// 	},
-
-// 	timer(){
-// 		this.timeInt = setInterval(() => {
-// 			this.timer++
-// 			if(this.timer % 30 === 0){
-// 				this.player.getAge()
-// 				if(this.player.age === 5){
-// 					}
-
-// 				}
-// 			}
-// 			if(this.timer % 15 === 0)
-// 		})
-// 	}
-
-//Increase pet's age every X minutes //***COMMIT***
-function age(name){
-	return window.setInterval (function(){
-		name.getAge()
-		console.log(name.age)
-	},1000)
-}
-
-//Increase pet's hunger hunger //***COMMIT***
-function eat(){
-	return window.setInterval (function(){
-		name.getHunger()
-		console.log(this.hunger)
-	},1000)
-}
+//Increase pet's hunger //***COMMIT***
+	function eat(){
+		return window.setInterval (function(){
+			name.getHunger()
+			this.start.hunger-- //APPLY TO OTHERS!!!!!
+			console.log(this.hunger)
+		},1500)
+	};
 
 //Increase sleepiness //***COMMIT***
-function sleep(){
-	return window.setInterval (function(){
-		name.getSleep()
-		console.log(this.sleep)
-	},1000)
-}
+	function sleep(){
+		return window.setInterval (function(){
+			name.getSleep()
+			console.log(this.sleep)
+		},1000)
+	};
 
 //Increase Boredom //***COMMIT***
-function bored(){
-	return window.setInterval (function(){
-		name.getBoredom()
-		console.log(this.boredom)
-	},1000)
+	function bored(){
+		return window.setInterval (function(){
+			name.getBoredom()
+			console.log(this.boredom)
+		},1000)
+	};
 }
+//---------Button Functions--------
 
-//Pet dies if hunger, boredom, or sleepiness reaches 0 //***COMMIT***
+//FEED ME --> MORPH pizza to strawberry
+const feedMe = document.querySelector("#feed")
+feedMe.addEventListener('click', (event) => {
+	let strawberry = document.querySelector("img")
+	strawberry.setAttribute("src", "Images/newstrawb.png")
+	start.eat()
 
-//Morph pet at certain ages //***COMMIT***
-
-//Have burgergotchi appear at night.
-
-const petAlive = window.setInterval(function(){
-	if ()
 })
+
+//Animate pet
+const move = document.querySelector("#play")
+move.addEventListener('click', (event) => {
+	let strawberry = document.querySelector("img")
+	strawberry.setAttribute("src", "Images/newstrawb.png")
+	strawberry.style.marginLeft = "50px"
+	strawberry.style.marginRight = "50px"
+})
+
+//Change background to dark blue -> Lights Off button
+const lightsOut = document.querySelector("#lights")
+lightsOut.addEventListener('click', (event) => {
+	let darkBackground = document.querySelector("#screen")
+	darkBackground.style.backgroundColor = 'navy'
+	document.body.style.backgroundImage = "url('Images/NorthernLights.jpeg')"
+	
+	let changeDevice = document.querySelector("#device")
+	changeDevice.style.backgroundColor = 'black'
+	start.sleep()
+})
+
+
+//---------End Game---------
+// /Pet dies if hunger, boredom, or sleepiness reaches 0 //***COMMIT***
+// const petAlive = (function(){
+// 	if age==10 || hunger=10 || sleep==10 || boredom==10{
+// 		this.alive == true
+// 		alert("GAME OVER!")
+// 	}
+// })
+
+//---------Setups-----------
 // const sleep = document.getElementById()
 // getName()
 
 let userInput = " "
 promptBox()
-const start = new Tamagotchi(userInput)
-let ageVariable = age(start)
-
+// const start = new Tamagotchi(userInput)
+// let ageVariable = age(start)
 
